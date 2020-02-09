c4db04aa3ba0a7a50d11619d6353784d18f630b4
wanarisu <wanarisu@gmail.com>
2020-02-10 02:48:41 +0900

    初期と＋

---
+++ Polish/Unity Project/Assets/Scripts/Camera_Control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+	/// <summary>
+	/// カメラの距離の名前用 enum型
+	/// </summary>
+	public enum CAMERA_POSITION_NAME
+	{
+		eFIRST_PERSON,			// 一人称視点
+		eSHORT_DISTANCE,		// 三人称視点(近距離)
+		eMEDIUM_DISTANCE,       // 三人称視点(中距離)
+		eLONG_DISTANCE,         // 三人称視点(遠距離)
+	}
+
+	[SerializeField, Tooltip("カメラを置きたい位置")] private Transform[] Camera_SpecifiedPositions;
+
+	/// <summary>
+	/// カメラのポジション指示
+	/// </summary>
+	/// <param name="cpn"> 移動したい位置 </param>
+	public void CameraPositionIndication(CAMERA_POSITION_NAME cpn)
+	{
+		transform.parent = Camera_SpecifiedPositions[(int)cpn];
+		transform.localPosition = Vector3.zero;
+		transform.localPosition = Vector3.zero;
+	}
+}
---
+++ Polish/Unity Project/Assets/Scripts/Debug.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour
+{
+	private Camera_Control DebugCamera;
+
+	private void Start()
+	{
+		DebugCamera = Camera.main.GetComponent<Camera_Control>();
+	}
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Keypad0))
+		{
+			DebugCamera.CameraPositionIndication((Camera_Control.CAMERA_POSITION_NAME)0);
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad1))
+		{
+			DebugCamera.CameraPositionIndication((Camera_Control.CAMERA_POSITION_NAME)1);
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad2))
+		{
+			DebugCamera.CameraPositionIndication((Camera_Control.CAMERA_POSITION_NAME)2);
+		}
+		if(Input.GetKeyDown(KeyCode.Keypad3))
+		{
+			DebugCamera.CameraPositionIndication((Camera_Control.CAMERA_POSITION_NAME)3);
+		}
+	}
+
+	private int Get_Number()
+	{
+		switch(Input.inputString)
+		{
+			case "Key.pad0":
+				return 0;
+			case "Key.pad1":
+				return 1;
+			case "Key.pad2":
+				return 2;
+			case "Key.pad3":
+				return 3;
+			case "Key.pad4":
+				return 4;
+			case "Key.pad5":
+				return 5;
+			case "Key.pad6":
+				return 6;
+			case "Key.pad7":
+				return 7;
+			case "Key.pad8":
+				return 8;
+			case "Key.pad9":
+				return 9;
+			case "Alpha0":
+				return 0;
+			case "Alpha1":
+				return 1;
+			case "Alpha2":
+				return 2;
+			case "Alpha3":
+				return 3;
+			case "Alpha4":
+				return 4;
+			case "Alpha5":
+				return 5;
+			case "Alpha6":
+				return 6;
+			case "Alpha7":
+				return 7;
+			case "Alpha8":
+				return 8;
+			case "Alpha9":
+				return 9;
+			default:
+				break;
+		}
+
+		return 99;
+	}
+}
